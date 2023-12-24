603144148c44e1954581816feedb66f42ecb0b9e
Salmon <sos2012happy@gmail.com>
2023-12-24 20:45:13 +0800

    feture:docker

--- flask_app/app.py
+++ flask_app/app.py
@@ -27,5 +27,5 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run(**app_config)
+# if __name__ == '__main__':
+#     app.run(**app_config)
